c84ae1f1aabe35535938dff50ff2f72acb847af9
yqnnq <105790761+yqnnq@users.noreply.github.com>
2022-09-06 23:42:45 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@
 
 $(function () {
     $('.hamburger').click(function() {
-        $(this).toggleClass('active');
+      $(this).toggleClass('active');
 
         if ($(this).hasClass('active')) {
-            $('.hamburger-nav').addClass('active');
+          $('.hamburger-nav').addClass('active');
+          $('.mask').addClass('active');
         } else {
-            $('.hamburger-nav').removeClass('active');
+          $('.hamburger-nav').removeClass('active');
+          $('.mask').removeClass('active');
         }
 
     });
@@ -16,7 +18,8 @@ $(function () {
 $(function() {
     $('.hamburger-nav a[href]').click(function() {
         $('.hamburger-nav').removeClass('active');
-        $('.hamburger').removeClass('active');
+      $('.hamburger').removeClass('active');
+      $('.mask').removeClass('active');
 
     });
 });
